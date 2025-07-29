aeba40f01f5b6a370091b92823fdefd6d06b8060
Akary López <akarylop@outlook.com>
2025-07-28 22:47:16 -0600

    old dash

---
+++ src/pages/NewHome/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Layout } from '../../components';
+
+
+function NewHome() {
+  return (
+    <Layout>
+        <div>
+fsfds
+        </div>
+    </Layout>
+  )
+}
+
+export default NewHome
