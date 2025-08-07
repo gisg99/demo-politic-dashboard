6b679b0136f55c0e5fc66c05f3119192aa1a08d0
Akary López <akarylop@outlook.com>
2025-08-07 12:37:09 -0600

    template pages

--- src/pages/Configuracion/index.jsx
+++ src/pages/Configuracion/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Layout } from '../../components';
+import { Layout, Card } from '../../components';
 
 
 function Configuracion() {
   return (
     <Layout>
       <div className='flex flex-col w-full h-full items-center py-4 px-6'>
-        <div className='w-full flex justify-center items-center'>
-          <h1 className='text-gray-400 font-bold uppercase '>Resumen ejecutivo</h1>
-         <h2 className='relative left-100 bg-[#acb8bf] px-3py-0.5 cursor-pointer text-white font-medium rounded-full hover:bg-tertiary'>Descargar</h2>
+        <div className='w-full flex justify-end items-center'>
+         <h2 className='relative  bg-[#acb8bf] px-3 py-0.5 cursor-pointer text-white font-medium rounded-full hover:bg-tertiary'>Descargar</h2>
         </div>
-        <div className='flex flex-col'>
-          
+        <div className='flex gap-2 w-full'>
+          <div className='w-[60%]'>
+            <Card title='Nivel de aceptación PL'>
+
+            </Card>
+          </div>
         </div>
       </div>
     </Layout>
